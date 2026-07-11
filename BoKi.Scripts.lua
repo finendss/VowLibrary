58ed41ccec1ee4c5770b945276870de4bbf63a3f
Vow <352602248@qq.com>
2026-07-12 00:32:54 +0800

    Update BoKi.Scripts.lua

--- BoKi.Scripts.lua
+++ BoKi.Scripts.lua
@@ -1,4 +1,9 @@
+--你好
+BoKi = {}
+BoKi.Scripts = "1018104807" 
+
 if BoKi.Scripts ~= "1018104807" then
-  game.Players.LocalPlayer:Kick("把脚本复制完整")
+    game.Players.LocalPlayer:Kick("把脚本复制完整")
 end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/finendss/VowLibrary/refs/heads/main/BoKi-obfuscated.lua"))()
